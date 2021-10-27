e15bd8097538e86f1fa80fd7f10028c7fff869ee
neoconstel <neoconstel@gmail.com>
2021-10-28 00:01:37 +0100

    shell executing commands WITH arguments (but only with full command path specified)

--- shell.c
+++ shell.c
@@ -1,8 +1,12 @@
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stddef.h>
 #include "tests/errors/errors.h"
+#include <string.h>
+#include "tests/utils/headers.h"
 
 /**
  * main - Entry point
@@ -16,6 +20,9 @@ int main()
 	we'll give it a size of 2048 */
 	char *buffer = NULL;
 	size_t buffer_size = 2048;
+	int wait_status;
+	pid_t child_pid;
+	int i;
 
 	buffer = (char *)calloc(sizeof(char), buffer_size);
 	if (buffer == NULL)
@@ -30,8 +37,35 @@ int main()
 	/* reading STDIN in loop */
 	while (getline(&buffer, &buffer_size, stdin) > 0)
 	{
+		/* IMPORTANT! -- nullify the newline character from buffer */
+		for (i = 0; buffer[i]; i++)
+		{
+			if (buffer[i] == '\n')
+			buffer[i] = '\0';
+		}
+
 		printf("cmd = %s\n", buffer);
 		write(1, "($) ", 4);
+
+		/* create fork for executing the command entered */
+		child_pid = fork();
+		if (child_pid == -1)  /* fork failure */
+		{
+			write(1, FORK_FAILED, strlen(FORK_FAILED) );
+		}
+
+		else if (child_pid > 0)  /* parent of fork */
+		{
+			wait(&wait_status);
+		}
+
+		else if (child_pid == 0)  /* child of fork */
+		{
+			/* execute the command entered */
+			char **args = split(buffer, " ");  /* get separate arguments */
+			execve(args[0], args, NULL);
+		}
+			
 	}
 
 	/* when the user types CTRL + D (abort) */
--- tests/errors/errors.h
+++ tests/errors/errors.h
@@ -3,5 +3,6 @@
 
 /* error messages */
 const char *MALLOC_FAILED = "Memory allocation failed";
+const char *FORK_FAILED = "Program fork failed";
 
 #endif /* ERRORS_H */
