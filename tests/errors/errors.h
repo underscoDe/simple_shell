c6f179ab42cbdf584bf487d1ad14439fcf611602
underscoDe <developer.awounou@gmail.com>
2021-10-29 13:48:18 +0100

    function to execute a command

--- tests/errors/errors.h
+++ tests/errors/errors.h
@@ -5,5 +5,7 @@
 const char *MALLOC_FAILED = "Memory allocation failed";
 const char *FORK_FAILED = "Program fork failed";
 const char *INVALID_COMMAND = "Invalid command";
+const char *FORK_ERROR = "Failed to duplicate process";
+const char *SHELL = "Shell";
 
 #endif /* ERRORS_H */
--- tests/utils/functions.c
+++ tests/utils/functions.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <signal.h>
 #include "headers.h"
+#include "../errors/errors.h"
 
 /**
  * split - splits command input into parts
@@ -53,4 +55,36 @@ void free_array(char **array)
 
 	free(array);
 	array = NULL;
+}
+
+/**
+ * exec_cmd - executes the given command
+ * @cmd: command to be executed
+ *
+ * Return: nothing
+ */
+void exec_cmd(char **cmd)
+{
+	pid_t pid = 0;
+	int status = 0;
+
+	/* we duplicate the current process */
+	pid = fork();
+	if (pid == -1)
+		perror(FORK_ERROR);
+	/* if fork() works, the parent process wait for its child */
+	else if (pid > 0)
+	{
+		/* we block the parent process until its child ends */
+		waitpid(pid, &status, 0);
+		/* then we kill the child */
+		kill(pid, SIGTERM);
+	}
+	else
+	{
+		/* child process executes the command or quit whether execve fails */
+		if (execve(cmd[0], cmd, NULL) == -1)
+			perror(SHELL);
+		exit(EXIT_FAILURE);
+	}
 }
