e17bf96ccaf0dab45e81d1f6169b0fd97e811f3f
underscoDe <developer.awounou@gmail.com>
2021-10-30 11:22:55 +0100

    pwd builtin

--- tests/utils/builtins.c
+++ tests/utils/builtins.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <linux/limits.h>
 
 /**
  * built_in_cd - changes directory
@@ -8,7 +9,23 @@
 */
 void	built_in_cd(char *path)
 {
-    if (chdir(path) == -1) {
-        perror("chdir()");
-    }
+	if (chdir(path) == -1) {
+		perror("chdir()");
+	}
+}
+
+/**
+ * built_in_pwd - prints the working directory
+ *
+ * Return: nothing
+*/
+void built_in_pwd(void)
+{
+	char cwd[PATH_MAX];
+
+	if (getcwd(cwd, sizeof(cwd)) != NULL) {
+		   printf("%s\n", cwd);
+	} else {
+		perror("getcwd()");
+	}
 }
