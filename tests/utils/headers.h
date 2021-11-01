5025814ca30600dc0e0154d470acb9bbea8483f0
underscoDe <developer.awounou@gmail.com>
2021-11-01 18:44:27 +0100

    update get_absolute_path func prototype

--- tests/utils/headers.h
+++ tests/utils/headers.h
@@ -4,6 +4,6 @@
 char **split(char *raw_cmd, char *limit);
 void free_array(char **array);
 void exec_cmd(char **cmd);
-void get_absolute_path(char **cmd);
+bool get_absolute_path(char **cmd);
 
 #endif /* HEADERS_H */
