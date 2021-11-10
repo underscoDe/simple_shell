5b174065dbe754c6ee1ce039d503312a933c5d7e
underscoDe <developer.awounou@gmail.com>
2021-11-10 08:23:46 +0100

    entry point

---
+++ main.c
@@ -0,0 +1,13 @@
+/**
+ * main - Entry point
+ *
+ * @argc: number of arguments
+ * @argv: arguments list
+ * @envp: environment
+ *
+ * Return: returns 0
+ */
+int main(int argc, char **argv, char **envp)
+{
+
+}
