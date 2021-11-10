4f6bb9c4f7c1b218695272ead4978edc6d881d24
underscoDe <developer.awounou@gmail.com>
2021-11-10 08:24:23 +0100

    handling signals

---
+++ signal_handler.c
@@ -0,0 +1,16 @@
+#include <signal.h>
+#include <unistd.h>
+#include "minishell.h"
+
+/**
+ * signal_handler - prints a new prompt upon a signal
+ * @fildes: file descriptor
+ */
+void signal_handler(int fildes)
+{
+    char *prompt = "\n($) ";
+
+    (void)fildes;
+    signal(SIGINT, signal_handler);
+    write(STDIN_FILENO, prompt, 3);
+}
