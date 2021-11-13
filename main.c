9ea4d7f7a9ec85ec12991f6262d37ca044d72e31
underscoDe <developer.awounou@gmail.com>
2021-11-13 19:48:14 +0100

    main

--- main.c
+++ main.c
@@ -1,14 +1,35 @@
 #include "minishell.h"
+#include <stddef.h>
+#include <signal.h>
+
 /**
- * main - Entry point
+ * main - entry point
  *
- * @argc: number of arguments
+ * @argc: number of arguments given to the program
  * @argv: arguments list
- * @envp: environment
  *
  * Return: returns the value of the last executed command
  */
-int main(int argc, char **argv, char **envp)
+int main(int argc, char **argv)
 {
+    int retval = 0, retnval;
+    int *retnval_ptr = &retnval;
+    char *prompt = "($) ", newline = "\n";
 
+    /* we get the program name in the variable */
+    /* name which is a global variable */
+    name = argv[0];
+    /* set history count to 1 */
+    hist = 1;
+    /* initialize aliases to NULL */
+    aliases = NULL;
+    /* send SIGINT signal to print a new prompt */
+    signal(SIGINT, signal_handler);
+
+    /* initialize retnval to 0 */
+    *retnval_ptr = 0;
+    /* get a copy of the environment variable */
+    environ = getenvcopy();
+
+    return (0);
 }
